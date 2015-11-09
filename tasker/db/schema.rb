cdf4a2d29f2b7a5627fd886ac0bead7b4c4a3379
Akriti Singh <akritisingh@berkeley.edu>
2015-11-08 18:01:43 -0800

    added User model

--- tasker/db/schema.rb
+++ tasker/db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109014818) do
+ActiveRecord::Schema.define(version: 20151109015440) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "body"
     t.boolean  "finished"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
