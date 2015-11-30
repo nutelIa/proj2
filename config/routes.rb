f48294932e58d6281d01db4fdf1d0571d82884d4
Natalia Layson <nlayson@berkeley.edu>
2015-11-30 12:33:49 -0800

    Figured out how to have a name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :users
   get 'tasks/new', to: 'tasks#new'
   resources :tasks
