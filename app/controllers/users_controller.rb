05eb0c60c3833a48295aa71b1ea6c82350297952
Natalia Layson <nlayson@berkeley.edu>
2015-12-02 17:35:28 -0800

    Akriti's UI changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		# @friendships = Friendship.find(params[:id])
+		# for friend in current_user.friends
+		# 	if friend = @user
+		# 		@not_following = true
+		# 		return @not_following
+		# if @user in current_user.friends
+		# 	@not_following = true
+		# @not_following = false
 	end
 
 	def index
